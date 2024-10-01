f5cec8c0dbb1485e3bf2b54a7aa43d56c1b1ceee
ayoshinin <ayodejisamson73@yahoo.com>
2024-10-01 04:34:28 +0000

    base code

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
+  }
+
+  return arrayEnd;
+}
+
